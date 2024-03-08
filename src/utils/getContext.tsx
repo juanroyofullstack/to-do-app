14fc7635426815c7e2ba3a30cf9a5ec85624063f
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2024-03-08 12:57:40 +0100

    add: type imprevement within the project

--- src/utils/getContext.tsx
+++ src/utils/getContext.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
 
-const CreatedProjectContext = React.createContext<{ created: boolean }>({ created: false });
+const CreatedProjectContext = React.createContext<[{ created: boolean }, React.Dispatch<React.SetStateAction<{ created: boolean }>>] | { created: boolean }>({ created: false });
 
 type Props = {
     children: string | JSX.Element | JSX.Element[];
   }
   
 function useAppContext () {
-    const context: any = React.useContext(CreatedProjectContext);
+    const context = React.useContext(CreatedProjectContext);
     if(context === undefined) {
         throw new Error('no context');
     }
  
-    const [appState, setAppState] = context;
+    const [appState, setAppState]: any = context;
     return [appState, setAppState];
     
 }
 
 function AppCreatedProvider(props: Props) {
-    const [appState, setAppState] = React.useState<React.SetStateAction<{ created: boolean } | undefined>>({ created: false });
-    const value: any = React.useMemo(() => [appState, setAppState], [appState]);
+    const [appState, setAppState] = React.useState<{ created: boolean }>({ created: false });
+    const value = React.useMemo<[{ created: boolean }, React.Dispatch<React.SetStateAction<{ created: boolean }>>]>(() => [appState, setAppState], [appState]);
 
     return (<CreatedProjectContext.Provider value={value} {...props} />);
 }
